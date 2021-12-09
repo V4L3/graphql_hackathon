0aaef86cbbedc3cbe840187f66055ea47be39919
Claudio Baumgartner <claudio.baumgartner@mgb.chmgb.ch>
2021-12-09 18:04:27 +0100

    Add no image example

---
+++ components/PokedexNoImage.js
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+
+const PokedexNoImage = ({data}) => {
+  return (
+    <>
+      {data?.pokemon_v2_pokemon &&
+        data.pokemon_v2_pokemon.map((pokemon, i) => (
+            <div style={{minHeight: '100vh', padding: '40px', ...getBackgroundColor(pokemon.pokemon_v2_pokemonspecy.pokemon_v2_pokemoncolor.name)}} key={i}>
+              <h3>{pokemon.name}</h3>
+              <p>{pokemon.base_experience}</p>
+              <p>{pokemon.height}</p>
+              <p>{pokemon.pokemon_v2_pokemonspecy?.pokemon_v2_pokemonspeciesflavortexts.reduce((acc, currentText) => {
+                return `${acc} ${currentText.flavor_text}`;
+              }, '') }</p>
+            </div>
+        ))}
+    </>
+  );
+};
+
+const getBackgroundColor = (colorString) => {
+  switch (colorString) {
+    case 'red':
+      return {backgroundColor: '#F3B286', color: 'black'};
+    case 'blue':
+      return {backgroundColor: '#5AA2B6', color: 'black'};
+    case 'yellow':
+      return {backgroundColor: '#DDC773', color: 'black'};
+    case 'green':
+      return {backgroundColor: '#93D1B4', color: 'black'};
+    case 'brown':
+      return {backgroundColor: '#CFBB9D', color: 'black'};
+    case 'purple':
+      return {backgroundColor: '#AC97D2', color: 'black'};
+    case 'pink':
+      return {backgroundColor: '#F6CFDA', color: 'black'};
+    case 'gray':
+      return {backgroundColor: '#CCCCCB', color: 'black'};
+    case 'white':
+      return {backgroundColor: '#EBEBED', color: 'black'};
+    default:
+      return {backgroundColor: '#363636', color: 'white'};
+  }
+};
+export default PokedexNoImage;
+
